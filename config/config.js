90db9e0da8f389b05ecb9b3273b9d3e0f10a474c
locemarn <marcelon27@gmail.com>
2019-05-20 17:51:25 -0300

    Configure sqlite3 and Sequelize database and adapt test to database

---
+++ config/config.js
@@ -0,0 +1,14 @@
+export default {
+  database: 'books',
+  username: '',
+  password: '',
+  params: {
+    dialect: 'sqlite',
+    storage: 'books.sqlite',
+    operatorsAliases: false,
+    freezeTableName: true,
+    define: {
+      underscored: true
+    }
+  }
+}
